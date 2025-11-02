71895bf4516a283393018c0e7742485b39e0cc83
jensstockhausen <jens@famst.de>
2025-11-02 13:10:30 +0100

    min JDK version required is now 17 cleanup of Main

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ group = "de.famst"
 version = "1.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -19,27 +19,42 @@ repositories {
 }
 
 val jfreeVersion = "3.2"
+
 val dcm4cheVersion = "3.3.8"
-val ioVersion = "2.6"
+
+val ioVersion = "2.14.0"
 val cliVersion = "1.4"
+
 val slf4jVersion = "1.7.25"
-val logbackVersion = "1.2.3"
-val junitVersion = "4.11"
+val logbackVersion = "1.5.19"
+
 val junit5Version = "5.0.1"
 val hamcrestVersion = "1.3"
 
 dependencies {
     implementation("org.jfree:jfreesvg:$jfreeVersion")
+
     implementation("org.dcm4che:dcm4che-core:$dcm4cheVersion")
+
     implementation("commons-io:commons-io:$ioVersion")
     implementation("commons-cli:commons-cli:$cliVersion")
+
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junit5Version")
+
     testImplementation("org.hamcrest:hamcrest-all:$hamcrestVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
 }
 
 application {
--- src/main/java/de/famst/dicom/visualizer/Main.java
+++ src/main/java/de/famst/dicom/visualizer/Main.java
@@ -1,108 +1,157 @@
 package de.famst.dicom.visualizer;
 
 import org.apache.commons.cli.*;
-import org.apache.commons.io.FileUtils;
 import org.jfree.graphics2d.svg.SVGGraphics2D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
- * Created by jens on 16.05.17.
+ * Main class for DICOM visualization tool.
+ * Processes DICOM files and generates SVG visualizations.
  */
 public class Main
 {
-  private static Logger LOG = LoggerFactory.getLogger(Main.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String args[])
+  private static final int BASE_HEIGHT = 70;
+  private static final int SERIES_SPACING = 20;
+
+  public static void main(String[] args)
   {
     LOG.info("Start");
 
-    Options options = new Options();
-    options.addOption("i", true, "input file");
-    options.addOption("p", true, "input path");
-    options.addOption("o", true, "output file");
-
+    Options options = createOptions();
     CommandLineParser parser = new DefaultParser();
 
     try
     {
       CommandLine cmd = parser.parse(options, args);
-
-      // one single dicom file
-      if ((cmd.hasOption("i") && cmd.hasOption("o")))
-      {
-        String in = cmd.getOptionValue("i");
-        String out = cmd.getOptionValue("o");
-
-        DicomParser dicomParser = DicomParser.parseFile(in);
-
-        int w = (int) dicomParser.length;
-        int h = 70;
-
-        SVGGraphics2D graph = new SVGGraphics2D(w, h);
-        DicomDrawer dicomDrawer = new DicomDrawer(dicomParser, graph, w, h);
-
-        LOG.info("Draw");
-
-        graph = (SVGGraphics2D) dicomDrawer.draw();
-
-        try
-        {
-          LOG.info("Save to [{}]", out);
-
-          String svgDocument = graph.getSVGDocument();
-          FileUtils.writeStringToFile(new File(out), svgDocument, StandardCharsets.UTF_8);
-        }
-        catch (IOException e)
-        {
-          e.printStackTrace();
-        }
-
-      }
-
-      // folder of dicom files
-      if ((cmd.hasOption("p") && cmd.hasOption("o")))
-      {
-        String in = cmd.getOptionValue("p");
-        String out = cmd.getOptionValue("o");
-
-        StudyDrawer studyDrawer = new StudyDrawer(in);
-
-        int w = (int) studyDrawer.maxLength;
-        int h = 70 * studyDrawer.files.size() + 20 * studyDrawer.series.size();
-
-        SVGGraphics2D graph = new SVGGraphics2D(w, h);
-
-        LOG.info("Draw");
-
-        graph = (SVGGraphics2D) studyDrawer.draw(graph);
-
-        try
-        {
-          LOG.info("Save to [{}]", out);
-
-          String svgDocument = graph.getSVGDocument();
-          FileUtils.writeStringToFile(new File(out), svgDocument, StandardCharsets.UTF_8);
-        }
-        catch (IOException e)
-        {
-          e.printStackTrace();
-        }
-
-      }
-
+      processCommand(cmd);
     }
     catch (ParseException e)
     {
-      LOG.error("Error ", e);
+      LOG.error("Error parsing command line arguments", e);
+      printUsage(options);
     }
 
     LOG.info("End");
   }
+
+  /**
+   * Creates command line options.
+   */
+  private static Options createOptions()
+  {
+    Options options = new Options();
+    options.addOption("i", "input", true, "input file");
+    options.addOption("p", "path", true, "input path");
+    options.addOption("o", "output", true, "output file");
+    return options;
+  }
+
+  /**
+   * Prints usage information.
+   */
+  private static void printUsage(Options options)
+  {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("dicom-visualizer", options);
+  }
+
+  /**
+   * Processes the command line arguments and delegates to appropriate handler.
+   */
+  private static void processCommand(CommandLine cmd)
+  {
+    if (cmd.hasOption("i") && cmd.hasOption("o"))
+    {
+      processSingleFile(cmd.getOptionValue("i"), cmd.getOptionValue("o"));
+    }
+    else if (cmd.hasOption("p") && cmd.hasOption("o"))
+    {
+      processFolder(cmd.getOptionValue("p"), cmd.getOptionValue("o"));
+    }
+    else
+    {
+      LOG.error("Invalid combination of options. Use -i/-o for single file or -p/-o for folder.");
+    }
+  }
+
+  /**
+   * Processes a single DICOM file and generates SVG output.
+   */
+  private static void processSingleFile(String inputPath, String outputPath)
+  {
+    try
+    {
+      DicomParser dicomParser = DicomParser.parseFile(inputPath);
+      int width = (int) dicomParser.length;
+      int height = BASE_HEIGHT;
+
+      SVGGraphics2D graph = new SVGGraphics2D(width, height);
+      DicomDrawer dicomDrawer = new DicomDrawer(dicomParser, graph, width, height);
+
+      LOG.info("Drawing single file");
+      graph = (SVGGraphics2D) dicomDrawer.draw();
+
+      saveSvg(graph, outputPath);
+    }
+    catch (Exception e)
+    {
+      LOG.error("Error processing single file: {}", inputPath, e);
+    }
+  }
+
+  /**
+   * Processes a folder of DICOM files and generates SVG output.
+   */
+  private static void processFolder(String inputPath, String outputPath)
+  {
+    try
+    {
+      StudyDrawer studyDrawer = new StudyDrawer(inputPath);
+      int width = (int) studyDrawer.maxLength;
+      int height = BASE_HEIGHT * studyDrawer.files.size() + SERIES_SPACING * studyDrawer.series.size();
+
+      SVGGraphics2D graph = new SVGGraphics2D(width, height);
+
+      LOG.info("Drawing folder");
+      graph = (SVGGraphics2D) studyDrawer.draw(graph);
+
+      saveSvg(graph, outputPath);
+    }
+    catch (Exception e)
+    {
+      LOG.error("Error processing folder: {}", inputPath, e);
+    }
+  }
+
+  /**
+   * Saves the SVG graphics to a file.
+   */
+  private static void saveSvg(SVGGraphics2D graph, String outputPath)
+  {
+    try
+    {
+      LOG.info("Saving to [{}]", outputPath);
+
+      String svgDocument = graph.getSVGDocument();
+      Path path = Paths.get(outputPath);
+      Files.writeString(path, svgDocument);
+
+      LOG.info("Successfully saved SVG to [{}]", outputPath);
+    }
+    catch (IOException e)
+    {
+      LOG.error("Error saving SVG to [{}]", outputPath, e);
+    }
+  }
+
 
 
 }
